231f6aeb871d4b64f6a19e509d6b4c4397be03bf
Dave <bigmanrunning@gmail.com>
2026-01-21 20:57:56 +0000

    Commit tom v2 01/21/26

--- tom.erl
+++ tom.erl
@@ -1,9 +1,15 @@
--module(tom).
--export([start/0, sendharry/0, senddick/0]).
+-module(tom).       %% define module, must match filename
+-export([start/0, sendharry/0, senddick/0]).    %% Export functions
 
+%% Function to spawn main loop as the server
 start() ->
+    %% If I'm not wrong, fun() -> is an anonymous function declaration
     Pid = spawn(fun() -> loop(undefined) end),
-    register(tom, Pid),
+    %% Register tom as the atom for this Pid
+    %% so that other processes can reach by name
+    register(tom, Pid),     
+    %% This returns the Pid tuple to caller
+    %% Essentially, it displays the tuple when started.
     Pid.
 
 
