339dbd703786f35e35562bbbf597e0ab6dc6a791
Dave <bigmanrunnign@gmail.com>
2026-01-24 10:34:38 -0600

    commit tom and README 01/24/26

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 I am learning Erlang Server programming.  As such, I have three servers called, Tom, Dick, and Harry.  These allow sending messages back and forth.  This first version is pretty pathetic, but I will build on it in future branches.
 
+1. main branch is the first one I wrote that functions.
+2. v2 is a branch with tom.erl heavily commented.
+
+
--- tom.erl
+++ tom.erl
@@ -12,19 +12,41 @@ start() ->
     %% Essentially, it displays the tuple when started.
     Pid.
 
-
+%%  Here is where the magic server action takes place
 loop(State) ->
+
+    %%  The receive/end block defines a server.
+    %%  loop() is just a function containing receive/end
+    %%  But, to be a server, it must also loop.
     receive
+
+        %% These are actions
+        %% They use arrows -> but don't get confused
+        %% When a received tuple matches one of these,
+        %% The action takes place (much like a case/switch).
         {From, get} ->
             From ! {reply, State},
+            %% Actions must call loop and give State as a parameter
+            %% Yes, it's recursive.
             loop(State);
+
+        %% This is the one doing real work
+        %% When a send() forwards a message to the appropriate
+        %% server, it sends a tuple (For reference, look at sendharry())
         {say, Text} ->
             io:format("Tom received: ~p~n", [Text]),
             loop(State);
+
+        %% If we send tom:stop(), we end the server.
         stop ->
             ok
     end.
 
+%% These functions handle sending messages to other servers.
+%% The Pid ! {say, Msg) does the work of sending
+%% Pid gets its target from the whereis().  
+%% Each server registers its name to the Erlang VM,
+%% so, these servers only work inside one VM.
 sendharry() ->
     Msg = setMessage(),
     Pid = whereis(harry), 
@@ -36,7 +58,10 @@ senddick() ->
     Pid ! {say, Msg}.
 
 
-
+%%  this function gets a line of text from the user
+%%  It's set aside to not repeat myself and make things
+%%  more readable.
 setMessage() ->
     Text = io:get_line("Enter text to send: "),
+    %% In Erlang, the last line of a function is the return value.
     string:trim(Text).
